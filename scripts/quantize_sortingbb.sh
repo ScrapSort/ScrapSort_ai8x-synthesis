38393f2ec5ab4600dcf28ae4cc52a45421340dc8
Geffen Cooper <geffenc2789@gmail.com>
2022-02-04 16:42:35 -0800

    latest bb model

--- scripts/quantize_sortingbb.sh
+++ scripts/quantize_sortingbb.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./quantize.py trained/sort_qat_bb2.pth.tar trained/sort_qat_bb2-q.pth.tar --device MAX78000 -v "$@"
+./quantize.py trained/sort_qat_bb_l1.pth.tar trained/sort_qat_bb_l1-q.pth.tar --device MAX78000 -v "$@"
